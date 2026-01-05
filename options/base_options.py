503a22bb69a6acf812a0e5aba07fc1aa7040864d
tabbyblack <tb789@cam.ac.uk>
2026-01-05 18:44:06 +0000

    .

--- options/base_options.py
+++ options/base_options.py
@@ -30,7 +30,8 @@ class BaseOptions():
         parser.add_argument('--checkpoints_dir', type=str, default='./checkpoints', help='models are saved here')
         # model parameters
         parser.add_argument('--model', type=str, default='cut', help='chooses which model to use.')
-        parser.add_argument('--input_nc', type=int, default=3, help='# of input image channels: 3 for RGB and 1 for grayscale')
+        # I have changed the default value of input_nc from 3 for rgb input to 275 for hyperspectral input
+        parser.add_argument('--input_nc', type=int, default=3, help='# of input image channels: 275 for RGB and 1 for grayscale')
         parser.add_argument('--output_nc', type=int, default=3, help='# of output image channels: 3 for RGB and 1 for grayscale')
         parser.add_argument('--ngf', type=int, default=64, help='# of gen filters in the last conv layer')
         parser.add_argument('--ndf', type=int, default=64, help='# of discrim filters in the first conv layer')
