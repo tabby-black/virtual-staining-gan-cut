aecf3836167587b3a9ac262473b6edcbdf0b5b37
tabbyblack <tb789@cam.ac.uk>
2026-01-20 16:56:31 +0000

    typo

--- scripts/train_cyclegan.sh
+++ scripts/train_cyclegan.sh
@@ -1,3 +1,3 @@
 set -ex
-python train.py --dataroot ./datasets/histology --name hsi_to_he_cyclegan --model cycle_gan --pool_size 50 --no_dropout  --use_wandb
+python train.py --dataroot ./datasets/histology --name hsi_to_rgb_cyclegan --model cycle_gan --pool_size 50 --no_dropout  --use_wandb
 ```
