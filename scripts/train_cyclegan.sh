06ad2320ddd43f29a9e0396762a9bae8407e3dc7
tabby-black <tb789@cam.ac.uk>
2026-01-24 17:29:43 +0000

    dataset_mode flag added to train_cyclegan.sh

--- scripts/train_cyclegan.sh
+++ scripts/train_cyclegan.sh
@@ -1,3 +1,3 @@
 set -ex
-python train.py --dataroot ./datasets/histology --name hsi_to_rgb_cyclegan --model cycle_gan --pool_size 50 --no_dropout
+python train.py --dataroot ./datasets/histology --dataset_mode hsi_unaligned --name hsi_to_rgb_cyclegan --model cycle_gan --pool_size 50 --no_dropout
 ```
