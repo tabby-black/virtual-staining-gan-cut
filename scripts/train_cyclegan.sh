c201810178eb4c0a88b60f089aeb01266ea597ce
tabby-black <tb789@cam.ac.uk>
2026-01-25 17:28:17 +0000

    disabled nce_idt in train_cyclegan.sh

--- scripts/train_cyclegan.sh
+++ scripts/train_cyclegan.sh
@@ -3,6 +3,6 @@ set -ex
 # have commented out CycleGAN training command for now to check if CUT training is error free
 #python train.py --dataroot ./datasets/histology --dataset_mode hsi_unaligned --name hsi_to_rgb_cyclegan --model cycle_gan --pool_size 50 --no_dropout
 
-python train.py --dataroot ./datasets/histology --dataset_mode hsi_unaligned --name hsi_to_rgb_CUT --CUT_mode CUT --pool_size 50 --no_dropout
+python train.py --dataroot ./datasets/histology --dataset_mode hsi_unaligned --name hsi_to_rgb_CUT --CUT_mode CUT --nce_idt 0 --pool_size 50 --no_dropout
 
 ```
