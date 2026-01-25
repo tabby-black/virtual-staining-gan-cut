c9bfffe7975586fb1f7bc151c9074b9319b7ab90
tabby-black <tb789@cam.ac.uk>
2026-01-25 16:56:45 +0000

    CUT training command in train_cyclegan.sh

--- scripts/train_cyclegan.sh
+++ scripts/train_cyclegan.sh
@@ -1,3 +1,7 @@
 set -ex
-python train.py --dataroot ./datasets/histology --dataset_mode hsi_unaligned --name hsi_to_rgb_cyclegan --model cycle_gan --pool_size 50 --no_dropout
+# have commented out CycleGAN training command for now to check if CUT training is error free
+#python train.py --dataroot ./datasets/histology --dataset_mode hsi_unaligned --name hsi_to_rgb_cyclegan --model cycle_gan --pool_size 50 --no_dropout
+
+python train.py --dataroot ./datasets/histology --name hsi_to_rgb_CUT --CUT_mode CUT
+
 ```
