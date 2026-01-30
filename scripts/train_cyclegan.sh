3b2935fe9eae8ada29612d9c14f6f0f54d97c9a3
tabby-black <tb789@cam.ac.uk>
2026-01-30 07:48:43 +0000

    changed training command back for CycleGAN

--- scripts/train_cyclegan.sh
+++ scripts/train_cyclegan.sh
@@ -1,8 +1,9 @@
 set -ex
 # --pool_size and --no_dropout flags have been added to example structure of training command
 # have commented out CycleGAN training command for now to check if CUT training is error free
-#python train.py --dataroot ./datasets/histology --dataset_mode hsi_unaligned --name hsi_to_rgb_cyclegan --model cycle_gan --pool_size 50 --no_dropout
+python train.py --dataroot ./datasets/histology --dataset_mode hsi_unaligned --name hsi_to_rgb_cyclegan --model cycle_gan --pool_size 50 --no_dropout
 
-python train.py --dataroot ./datasets/histology --dataset_mode hsi_unaligned --name hsi_to_rgb_CUT --CUT_mode CUT --nce_idt 0 --pool_size 50 --no_dropout
+# have commented out CUT training command for now - trying to train CycleGAN model
+#python train.py --dataroot ./datasets/histology --dataset_mode hsi_unaligned --name hsi_to_rgb_CUT --CUT_mode CUT --nce_idt 0 --pool_size 50 --no_dropout
 
 ```
