a4673d997851ee38ee352bc39ca82c0f34cc8ff3
tabby-black <tb789@cam.ac.uk>
2026-02-03 12:01:58 +0000

    added aspect ratio argument

--- options/test_options.py
+++ options/test_options.py
@@ -15,6 +15,9 @@ class TestOptions(BaseOptions):
         parser.add_argument('--eval', action='store_true', help='use eval mode during test time.')
         parser.add_argument('--num_test', type=int, default=50, help='how many test images to run')
 
+        # added aspect ratio argument
+        parser.add_argument('--aspect_ratio', type=float, default=1.0, help='aspect ratio of result images')
+
         # To avoid cropping, the load_size should be the same as crop_size
         parser.set_defaults(load_size=parser.get_default('crop_size'))
         self.isTrain = False
