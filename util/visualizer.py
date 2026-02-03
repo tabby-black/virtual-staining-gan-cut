dd0e4d9f1ef12c735642b51ec3f63ec7c77a7e56
tabby-black <tb789@cam.ac.uk>
2026-02-03 12:06:16 +0000

    only save rgb domain images

--- util/visualizer.py
+++ util/visualizer.py
@@ -36,14 +36,18 @@ def save_images(webpage, visuals, image_path, aspect_ratio=1.0, width=256):
     ims, txts, links = [], [], []
 
     for label, im_data in visuals.items():
-        im = util.tensor2im(im_data)
-        image_name = '%s/%s.png' % (label, name)
-        os.makedirs(os.path.join(image_dir, label), exist_ok=True)
-        save_path = os.path.join(image_dir, image_name)
-        util.save_image(im, save_path, aspect_ratio=aspect_ratio)
-        ims.append(image_name)
-        txts.append(label)
-        links.append(image_name)
+        if image.ndim == 3 and image.shape[0] in [1, 3]:
+            im = util.tensor2im(im_data)
+            image_name = '%s/%s.png' % (label, name)
+            os.makedirs(os.path.join(image_dir, label), exist_ok=True)
+            save_path = os.path.join(image_dir, image_name)
+            util.save_image(im, save_path, aspect_ratio=aspect_ratio)
+            ims.append(image_name)
+            txts.append(label)
+            links.append(image_name)
+        else:
+            # skip hyperspectral or unsupported tensors
+            continue
     webpage.add_images(ims, txts, links, width=width)
 
 
