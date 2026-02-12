8b79f83a5e9c6a2ddeaac6b6570df3328953055b
tabby-black <tb789@cam.ac.uk>
2026-02-12 15:12:03 +0000

    changed to CUT training command

--- scripts/train_cyclegan.sh
+++ scripts/train_cyclegan.sh
@@ -2,10 +2,9 @@ set -ex
 # --pool_size and --no_dropout flags have been added to example structure of training command
 # have commented out CycleGAN training command for now to check if CUT training is error free
 # have disabled visdom for now
-# have removed --num_threads 0 because training wouldn't start
-python train.py --dataroot ./datasets/histology --dataset_mode hsi_unaligned --name hsi_to_rgb_cyclegan_run2.1 --model cycle_gan --gpu_ids 3 --lr_policy linear --n_epochs 5 --n_epochs_decay 15 --pool_size 50 --no_dropout --lambda_identity 0 --batch_size 1 --preprocess resize_and_crop --load_size 286 --crop_size 256 --display_id -1 --print_freq 100 --display_freq 1000 --update_html_freq 1000 --save_epoch_freq 5 --no_html
+#python train.py --dataroot ./datasets/histology --dataset_mode hsi_unaligned --name hsi_to_rgb_cyclegan_run2.1 --model cycle_gan --gpu_ids 3 --lr_policy linear --n_epochs 5 --n_epochs_decay 15 --pool_size 50 --no_dropout --lambda_identity 0 --batch_size 1 --preprocess resize_and_crop --load_size 286 --crop_size 256 --display_id -1 --print_freq 100 --display_freq 1000 --update_html_freq 1000 --save_epoch_freq 5 --no_html
 
 # have commented out CUT training command for now - trying to train CycleGAN model
-#python train.py --dataroot ./datasets/histology --dataset_mode hsi_unaligned --name hsi_to_rgb_CUT --CUT_mode CUT --nce_idt False --nce_layers 4,8,12,16 --pool_size 50 --no_dropout
+python train.py --dataroot ./datasets/histology --dataset_mode hsi_unaligned --name hsi_to_rgb_CUT --CUT_mode CUT --nce_idt False --nce_layers 4,8,12,16 --pool_size 50 --no_dropout
 
 ```
