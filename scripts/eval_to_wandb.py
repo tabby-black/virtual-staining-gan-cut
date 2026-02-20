d605bca8cb18ec1483c97c73e82c2f2b0e0e6119
tabby-black <tb789@cam.ac.uk>
2026-02-20 16:10:44 +0000

    eval train run2

--- scripts/eval_to_wandb.py
+++ scripts/eval_to_wandb.py
@@ -39,11 +39,11 @@ def evaluate_epoch(results_dir, data_range=255.0, channel_axis=2):
     # return the average of the metrics for this epoch to be logged in wandb
     # replace eval_val with eval_train when testing on training dataset
     return {
-        "eval_val/n_images": int(len(ssim_scores)),
-        "eval_val/ssim_mean": float(np.mean(ssim_scores)) if ssim_scores else float("nan"),
-        "eval_val/ssim_std": float(np.std(ssim_scores)) if ssim_scores else float("nan"),
-        "eval_val/psnr_mean": float(np.mean(psnr_scores)) if psnr_scores else float("nan"),
-        "eval_val/psnr_std": float(np.std(psnr_scores)) if psnr_scores else float("nan"),
+        "eval_train/n_images": int(len(ssim_scores)),
+        "eval_train/ssim_mean": float(np.mean(ssim_scores)) if ssim_scores else float("nan"),
+        "eval_train/ssim_std": float(np.std(ssim_scores)) if ssim_scores else float("nan"),
+        "eval_train/psnr_mean": float(np.mean(psnr_scores)) if psnr_scores else float("nan"),
+        "eval_train/psnr_std": float(np.std(psnr_scores)) if psnr_scores else float("nan"),
     }
 
 def log_sample_images(results_dir, epoch, num_samples=5):
@@ -75,7 +75,7 @@ def log_sample_images(results_dir, epoch, num_samples=5):
     # log 5 sample real vs generated image pairs for each epoch
     # same 5 real images each time for fair comparison
     # replace eval_val_samples_epoch with eval_train_samples when testing on training dataset
-    wandb.log({f"eval_val_samples_epoch_{epoch}": table}, step=epoch)
+    wandb.log({f"eval_train_samples_epoch_{epoch}": table}, step=epoch)
 
 def run_test_py(repo_root, name, dataroot, epoch, results_dir, extra_args=None):
     # add --phase train to this command too to get results of testing on training set alongside validation set
@@ -102,13 +102,13 @@ def run_test_py(repo_root, name, dataroot, epoch, results_dir, extra_args=None):
 
 if __name__ == "__main__":
     repo_root = "/local/scratch-3/tb789/projects/virtual-staining-gan-cut"
-    experiment_name = "hsi_to_rgb_cyclegan_run2.1"
+    experiment_name = "hsi_to_rgb_cyclegan_run2"
     # so run this from /scripts
     dataroot = "./datasets/histology/"
     results_root = Path(repo_root) / "results" / experiment_name
     # max epoch 100
-    # max epoch for run2.1 = 80
-    epochs = list(range(5, 81, 5))
+    # max epoch for run2 = 85
+    epochs = list(range(5, 86, 5))
     wandb_project = "hyperspectral_image_reconstruction"
     # set to training run id each time so I can see metrics on the same run
     # currently set to id of run2 i.e. cyclegan_initial
