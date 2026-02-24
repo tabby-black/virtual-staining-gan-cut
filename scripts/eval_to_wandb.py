e37089a0c8106af6066818072c20fad5ac2f623c
tabby-black <tb789@cam.ac.uk>
2026-02-24 21:40:37 +0000

    eval bs3 train

--- scripts/eval_to_wandb.py
+++ scripts/eval_to_wandb.py
@@ -102,13 +102,13 @@ def run_test_py(repo_root, name, dataroot, epoch, results_dir, extra_args=None):
 
 if __name__ == "__main__":
     repo_root = "/local/scratch-3/tb789/projects/virtual-staining-gan-cut"
-    experiment_name = "hsi_to_rgb_cyclegan_run2.1"
+    experiment_name = "hsi_to_rgb_cyclegan_bs3"
     # so run this from /scripts
     dataroot = "./datasets/histology/"
     results_root = Path(repo_root) / "results" / experiment_name
     # max epoch 100
-    # max epoch for run2.1 = 80
-    epochs = list(range(5, 81, 5))
+    # max epoch for runbs3 = 60
+    epochs = list(range(5, 61, 5))
     wandb_project = "hyperspectral_image_reconstruction"
     # set to training run id each time so I can see metrics on the same run
     # currently set to id of run2 i.e. cyclegan_initial
