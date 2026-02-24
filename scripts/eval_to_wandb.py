bce35a461640269f666a2f5be6cba4774ca55323
tabby-black <tb789@cam.ac.uk>
2026-02-24 17:25:32 +0000

    .

--- scripts/eval_to_wandb.py
+++ scripts/eval_to_wandb.py
@@ -102,7 +102,7 @@ def run_test_py(repo_root, name, dataroot, epoch, results_dir, extra_args=None):
 
 if __name__ == "__main__":
     repo_root = "/local/scratch-3/tb789/projects/virtual-staining-gan-cut"
-    experiment_name = "hsi_to_rgb_cyclegan_bs3"
+    experiment_name = "hsi_to_rgb_cyclegan_run2"
     # so run this from /scripts
     dataroot = "./datasets/histology/"
     results_root = Path(repo_root) / "results" / experiment_name
