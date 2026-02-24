1d267cdb1ef4da059223880b5d8dd008e55322d0
tabby-black <tb789@cam.ac.uk>
2026-02-24 14:57:17 +0000

    .

--- train.py
+++ train.py
@@ -44,7 +44,7 @@ if __name__ == '__main__':
             project=opt.wandb_project_name,
             #entity=opt.wandb_entity,
             name=opt.name,
-            id = "om88iqhb",
+            #id = "om88iqhb",
             config=vars(opt),
             #resume="allow"
         )
