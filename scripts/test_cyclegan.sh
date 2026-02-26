b3fdb0a61f3d69ef4d6d04292c5dd8083611e7b4
tabby-black <tb789@cam.ac.uk>
2026-02-26 22:05:42 +0000

    .

--- scripts/test_cyclegan.sh
+++ scripts/test_cyclegan.sh
@@ -2,4 +2,5 @@ set -ex
 #python test.py --dataroot ./datasets/histology --name hsi_to_he_cyclegan --model cycle_gan --phase test --no_dropout
 
 # test command to test model with weights from 5th epoch
-python test.py --dataroot ./datasets/histology --dataset_mode hsi_unaligned --name hsi_to_rgb_cyclegan_run2 --model cycle_gan --gpu_ids 3 --epoch str(ep) --num_threads 0 --preprocess resize_and_crop --load_size 286 --crop_size 256 --no_dropout
+# CHANGE THIS LINE
+python test.py --dataroot ./datasets/histology --dataset_mode hsi_unaligned --name hsi_to_rgb_cyclegan_bs3 --model cycle_gan --gpu_ids 3 --epoch str(ep) --num_threads 0 --preprocess resize_and_crop --load_size 286 --crop_size 256 --no_dropout
