6ed67d5d05f2ee92e9fc854228fc3523c12f3d77
tabby-black <tb789@cam.ac.uk>
2026-02-27 12:50:07 +0000

    eval run2.1 train

--- scripts/eval_to_wandb.py
+++ scripts/eval_to_wandb.py
@@ -86,6 +86,7 @@ def run_test_py(repo_root, experiment_name, dataroot, ep, results_dir):
         "--model", "cycle_gan",
         "--dataset_mode", "hsi_unaligned",
         "--epoch", str(ep),
+       # CHANGE THIS LINE
         # only include this line for eval on training dataset
         "--phase", "train",
         "--results_dir", str(results_dir),
@@ -118,14 +119,14 @@ def run_test_py(repo_root, experiment_name, dataroot, ep, results_dir):
 if __name__ == "__main__":
     repo_root = "/local/scratch-3/tb789/projects/virtual-staining-gan-cut"
     # CHANGE THIS LINE
-    experiment_name = "hsi_to_rgb_cyclegan_run2"
+    experiment_name = "hsi_to_rgb_cyclegan_run2.1"
     # so run this from /scripts
     dataroot = "./datasets/histology/"
     results_root = Path(repo_root) / "results" / experiment_name
     # max epoch 100
-    # max epoch for run2 = 85
+    # max epoch for run2.1 = 80
     # CHANGE THIS LINE
-    epochs = list(range(5, 86, 5))
+    epochs = list(range(5, 81, 5))
     wandb_project = "hyperspectral_image_reconstruction"
     # set to training run id each time so I can see metrics on the same run
     # currently set to id of run2 i.e. cyclegan_initial
