2c41751569e27ffd63bcc3ea23de357e6590b6fe
tabby-black <tb789@cam.ac.uk>
2026-03-03 22:07:06 +0000

    start run5 training

--- scripts/train_cyclegan.sh
+++ scripts/train_cyclegan.sh
@@ -2,9 +2,9 @@ set -ex
 # --pool_size and --no_dropout flags have been added to example structure of training command
 # have commented out CycleGAN training command for now to check if CUT training is error free
 # have disabled visdom for now
-#python train.py --dataroot ./datasets/histology --dataset_mode hsi_unaligned --name hsi_to_rgb_cyclegan_bs3 --model cycle_gan --use_wandb --wandb_project_name hyperspectral_image_reconstruction --gpu_ids 3 --num_threads 0 --pool_size 50 --no_dropout --lambda_identity 0 --batch_size 3 --preprocess resize_and_crop --load_size 286 --crop_size 256 --display_id -1 --print_freq 100 --display_freq 1000 --save_epoch_freq 5 --no_html --amp
+python train.py --dataroot ./datasets/histology --dataset_mode hsi_unaligned --name hsi_to_rgb_cyclegan_run5 --model cycle_gan --use_wandb --wandb_project_name hyperspectral_image_reconstruction --gpu_ids 0 --num_threads 0 --pool_size 50 --no_dropout --lambda_identity 0 --lambda_A 5 --lambda_B 5 --batch_size 3 --preprocess resize_and_crop --load_size 286 --crop_size 256 --display_id -1 --print_freq 100 --display_freq 1000 --save_epoch_freq 5 --no_html --amp
 # have commented out CUT training command for now - trying to train CycleGAN model
 
 # get initial CUT training underway!
 # expecting some debugging to get dual encoder to work
-python train.py --dataroot ./datasets/histology --dataset_mode hsi_unaligned --name hsi_to_rgb_CUT1 --CUT_mode CUT --continue_train --epoch 50 --epoch_count 51 --use_wandb --wandb_project_name hyperspectral_image_reconstruction --nce_idt False --gpu_ids 1 --nce_layers 4,8,12,16 --pool_size 50 --no_dropout --print_freq 100 --display_id -1 --num_threads 0 --no_html
+#python train.py --dataroot ./datasets/histology --dataset_mode hsi_unaligned --name hsi_to_rgb_CUT1 --CUT_mode CUT --continue_train --epoch 50 --epoch_count 51 --use_wandb --wandb_project_name hyperspectral_image_reconstruction --nce_idt False --gpu_ids 1 --nce_layers 4,8,12,16 --pool_size 50 --no_dropout --print_freq 100 --display_id -1 --num_threads 0 --no_html
