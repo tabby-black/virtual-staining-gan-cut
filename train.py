f50492769a222833626b45f4fc895e156c0d8d38
tabby-black <tb789@cam.ac.uk>
2026-03-03 16:18:09 +0000

    CUT training

--- train.py
+++ train.py
@@ -44,7 +44,7 @@ if __name__ == '__main__':
             project=opt.wandb_project_name,
             #entity=opt.wandb_entity,
             name=opt.name,
-            id = "8xtrbsng",
+            id = "s9hui43n",
             config=vars(opt),
             resume="allow"
         )
