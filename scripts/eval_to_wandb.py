0c65c993efcd5202a7535fd60c6b7683065826be
tabby-black <tb789@cam.ac.uk>
2026-03-09 16:33:25 +0000

    eval run5 on val

--- scripts/eval_to_wandb.py
+++ scripts/eval_to_wandb.py
@@ -136,14 +136,14 @@ def run_test_py(repo_root, experiment_name, dataroot, ep, results_dir):
 if __name__ == "__main__":
     repo_root = "/local/scratch-3/tb789/projects/virtual-staining-gan-cut"
     # CHANGE THIS LINE
-    experiment_name = "hsi_to_rgb_cyclegan_run2.1"
+    experiment_name = "hsi_to_rgb_cyclegan_run5"
     # so run this from /scripts
     dataroot = "./datasets/histology/"
     results_root = Path(repo_root) / "results" / experiment_name
     # max epoch 100
-    # fill in for epochs 45 - 110 in bs4 on val
+    # run5 epochs: 0-48
     # CHANGE THIS LINE
-    epochs = list(range(5, 80, 5))
+    epochs = list(range(5, 46, 5))
     wandb_project = "hyperspectral_image_reconstruction"
     # set to training run id each time so I can see metrics on the same run
     # currently set to id of run2 i.e. cyclegan_initial
