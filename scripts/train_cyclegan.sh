8fb653d101dbc9e280fa0f24a677469ddf9f1fcb
tabby-black <tb789@cam.ac.uk>
2026-03-28 08:15:06 +0000

    .

--- scripts/train_cyclegan.sh
+++ scripts/train_cyclegan.sh
@@ -4,7 +4,7 @@ set -ex
 # have disabled visdom for now
 
 # new cycleGAN run6.1 command - learning rate reduced by one order of magnitude
-python train.py --dataroot ./datasets/histology --dataset_mode hsi_unaligned --name hsi_to_rgb_cyclegan_run6.1 --model cycle_gan --continue_train --epoch 6 --epoch_count 7 --use_wandb --wandb_project_name hyperspectral_image_reconstruction --gpu_ids 1 --num_threads 0  --pool_size 50 --no_dropout --lambda_identity 0 --lr 0.00002 --batch_size 3 --preprocess resize_and_crop --load_size 286 --crop_size 256 --display_id -1 --print_freq 100 --display_freq 1000 --save_epoch_freq 1 --no_html --amp
+python train.py --dataroot ./datasets/histology --dataset_mode hsi_unaligned --name hsi_to_rgb_cyclegan_run6.1 --model cycle_gan --continue_train --epoch 6 --epoch_count 7 --use_wandb --wandb_project_name hyperspectral_image_reconstruction --gpu_ids 3 --num_threads 0  --pool_size 50 --no_dropout --lambda_identity 0 --lr 0.00002 --batch_size 3 --preprocess resize_and_crop --load_size 286 --crop_size 256 --display_id -1 --print_freq 100 --display_freq 1000 --save_epoch_freq 1 --no_html --amp
 
 # cyclegan bs8 run command
 #python train.py --dataroot ./datasets/histology --dataset_mode hsi_unaligned --name hsi_to_rgb_cyclegan_bs8 --model cycle_gan --use_wandb --wandb_project_name hyperspectral_image_reconstruction --gpu_ids 1 --num_threads 0 --pool_size 50 --no_dropout --lambda_identity 0 --batch_size 8 --preprocess resize_and_crop --load_size 286 --crop_size 160 --display_id -1 --print_freq 100 --display_freq 1000 --save_epoch_freq 1 --no_html --amp
