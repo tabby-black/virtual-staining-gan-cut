47ffded8cac70661e1d3d9169480807b59c03c53
tabby-black <tb789@cam.ac.uk>
2026-03-29 21:00:50 +0100

    mask visualisation notebook

--- train.py
+++ train.py
@@ -87,8 +87,8 @@ if __name__ == '__main__':
                 model.parallelize()
             model.set_input(data)  # unpack data from dataset and apply preprocessing
             # print to see tensor shapes to see if crop size is being applied correctly
-            print("real_A shape:", model.real_A.shape)
-            print("real_B shape:", model.real_B.shape)
+            #print("real_A shape:", model.real_A.shape)
+            #print("real_B shape:", model.real_B.shape)
             model.optimize_parameters()   # calculate loss functions, get gradients, update network weights
             if len(opt.gpu_ids) > 0:
                 torch.cuda.synchronize()
