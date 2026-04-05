5e38d65ebe8663811d5c2cc9463e5b96334dc5ba
tabby-black <tb789@cam.ac.uk>
2026-04-05 11:54:08 +0100

    prep for first CUT training run with new data loader

--- scripts/train_cyclegan.sh
+++ scripts/train_cyclegan.sh
@@ -4,7 +4,7 @@ set -ex
 # have disabled visdom for now
 
 # new cycleGAN run6.1 command - learning rate reduced by one order of magnitude
-python train.py --dataroot ./datasets/histology --dataset_mode hsi_unaligned --name hsi_to_rgb_cyclegan_run6.1 --model cycle_gan --continue_train --epoch 20 --epoch_count 21 --use_wandb --wandb_project_name hyperspectral_image_reconstruction --gpu_ids 1 --num_threads 0  --pool_size 50 --no_dropout --lambda_identity 0 --lr 0.00002 --batch_size 3 --preprocess resize_and_crop --load_size 286 --crop_size 256 --display_id -1 --print_freq 100 --display_freq 1000 --save_epoch_freq 1 --no_html --amp
+#python train.py --dataroot ./datasets/histology --dataset_mode hsi_unaligned --name hsi_to_rgb_cyclegan_run6.1 --model cycle_gan --continue_train --epoch 20 --epoch_count 21 --use_wandb --wandb_project_name hyperspectral_image_reconstruction --gpu_ids 1 --num_threads 0  --pool_size 50 --no_dropout --lambda_identity 0 --lr 0.00002 --batch_size 3 --preprocess resize_and_crop --load_size 286 --crop_size 256 --display_id -1 --print_freq 100 --display_freq 1000 --save_epoch_freq 1 --no_html --amp
 
 # cyclegan bs8 run command
 #python train.py --dataroot ./datasets/histology_full --dataset_mode hsi_unaligned --name hsi_to_rgb_cyclegan_bs8 --model cycle_gan --continue_train --epoch 36 --epoch_count 37 --use_wandb --wandb_project_name hyperspectral_image_reconstruction --gpu_ids 0 --num_threads 0 --pool_size 50 --no_dropout --lambda_identity 0 --batch_size 8 --preprocess resize_and_crop --load_size 286 --crop_size 160 --display_id -1 --print_freq 100 --display_freq 1000 --save_epoch_freq 1 --no_html --amp
@@ -17,5 +17,6 @@ python train.py --dataroot ./datasets/histology --dataset_mode hsi_unaligned --n
 
 # get initial CUT training underway!
 # expecting some debugging to get dual encoder to work
-#python train.py --dataroot ./datasets/histology_full --dataset_mode hsi_unaligned --name hsi_to_rgb_CUT2 --CUT_mode CUT --use_wandb --wandb_project_name hyperspectral_image_reconstruction --nce_idt False --gpu_ids 1 --nce_layers 4,8,12,16 --pool_size 50 --no_dropout --print_freq 100 --display_id -1 --num_threads 0 --no_html
+# I am wokring on starting a new CUT run with the new dataloader 
+python train.py --dataroot ./datasets/histology_full --dataset_mode hsi_unaligned --patch_mode overlapping --name hsi_to_rgb_CUT3 --CUT_mode CUT --use_wandb --wandb_project_name hyperspectral_image_reconstruction --nce_idt False --gpu_ids 1 --nce_layers 4,8,12,16 --pool_size 50 --no_dropout --print_freq 100 --save_epoch_freq 1 --display_id -1 --num_threads 0 --no_html
 # --continue_train --epoch 75 --epoch_count 76
