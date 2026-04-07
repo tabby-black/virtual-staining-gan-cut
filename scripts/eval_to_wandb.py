4bdc57d46b81be4dbe00017be3982b9e7fb11e99
tabby-black <tb789@cam.ac.uk>
2026-04-07 21:58:32 +0100

    big fix

--- scripts/eval_to_wandb.py
+++ scripts/eval_to_wandb.py
@@ -58,8 +58,8 @@ def evaluate_epoch(results_dir, data_range=255.0, channel_axis=2):
             continue
         
         # compute patch-level metrics
-        # removed channel_axis arguments
-        patch_ssim = ssim(fake_img, real_img, data_range=data_range)
+        # removed channel_axis argument from PSNR
+        patch_ssim = ssim(fake_img, real_img, channel_axis=channel_axis, data_range=data_range)
         patch_psnr = psnr(fake_img, real_img, data_range=data_range)
 
         # group by original image
