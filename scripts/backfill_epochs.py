1267b23b6f15b978ba3bcbdd2b3bf4214af48f1e
tabby-black <tb789@cam.ac.uk>
2026-04-09 10:07:27 +0100

    rep1

--- scripts/backfill_epochs.py
+++ scripts/backfill_epochs.py
@@ -4,7 +4,7 @@ import wandb
 
 
 PROJECT = "hyperspectral_image_reconstruction"
-RUN_NAME = "CUT3_backfilled"
+RUN_NAME = "CUT4_backfilled"
 LOSS_LOG = "checkpoints/hsi_to_rgb_CUT4/loss_log.txt"
 
 # Parse: (epoch: 1, iters: 700, time: ..., data: ...) D_A: 0.2 G_A: 0.3 ...
