f9e3f80a7c69ccbc17641b1f44bf1ed1374f8a7e
tabby-black <tb789@cam.ac.uk>
2026-04-16 13:33:01 +0100

    copy tensors after rotations

--- data/hsi_unaligned_dataset.py
+++ data/hsi_unaligned_dataset.py
@@ -480,8 +480,8 @@ class HSIUnalignedDataset(BaseDataset):
                 # random rotation - apply to both hsi and rgb 
                 # commented out for now because working on runs without augmentation
                 k = np.random.randint(0,4)
-                A = np.rot90(A, k)
-                B = np.rot90(B, k)
+                A = np.rot90(A, k).copy()
+                B = np.rot90(B, k).copy()
 
                 # gaussian noise - apply to hsi only
                 # 50% chance that gaussian noise is applied
