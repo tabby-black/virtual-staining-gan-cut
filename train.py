324cfb8105ab3add1255773afd609528653cde34
tabby-black <tb789@cam.ac.uk>
2026-04-16 09:40:00 +0100

    resume rep2 training

--- train.py
+++ train.py
@@ -44,7 +44,7 @@ if __name__ == '__main__':
             project=opt.wandb_project_name,
             #entity=opt.wandb_entity,
             name=opt.name,
-            id = "byocjqij",
+            id = "atsqoj5o",
             config=vars(opt),
             resume="allow"
         )
