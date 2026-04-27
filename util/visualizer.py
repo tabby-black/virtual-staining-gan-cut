0cb6420ab5d5e0bf0fed6c0c566be63e26b9553d
tabby-black <tb789@cam.ac.uk>
2026-04-27 13:56:37 +0100

    only save rgb images

--- util/visualizer.py
+++ util/visualizer.py
@@ -32,6 +32,10 @@ def save_images(webpage, visuals, image_path, aspect_ratio=1.0, width=256):
     ims, txts, links = [], [], []
 
     for label, im_data in visuals.items():
+        # only save RGB images
+        if label not in ["real_B", "fake_B"]:
+            continue
+        
         im = util.tensor2im(im_data)
         image_name = '%s/%s.png' % (label, name)
         os.makedirs(os.path.join(image_dir, label), exist_ok=True)
