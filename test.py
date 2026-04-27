d5bfecca55eb9389e6d1569df97b00c89f78fdca
tabby-black <tb789@cam.ac.uk>
2026-04-27 13:53:56 +0100

    .

--- test.py
+++ test.py
@@ -52,7 +52,7 @@ if __name__ == '__main__':
     opt.batch_size = 1    # test code only supports batch_size = 1
     opt.serial_batches = True  # disable data shuffling; comment this line if results on randomly chosen images are needed.
     opt.no_flip = True    # no flip; comment this line if results on flipped images are needed.
-    #opt.display_id = -1   # no visdom display; the test code saves the results to a HTML file.
+    opt.display_id = -1   # no visdom display; the test code saves the results to a HTML file.
     dataset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
     # this line is not used later in the code!
     #train_dataset = create_dataset(util.copyconf(opt, phase="train"))
