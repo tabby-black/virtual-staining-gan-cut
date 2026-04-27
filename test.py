3c1f2b4330d3cc2d140e6ca17588d322be8f01ea
tabby-black <tb789@cam.ac.uk>
2026-04-27 12:04:45 +0100

    remove test image limit

--- test.py
+++ test.py
@@ -93,8 +93,10 @@ if __name__ == '__main__':
             if opt.eval:
                 model.eval()
         
-        if i >= opt.num_test:  # only apply our model to opt.num_test images.
-            break
+        # removed restriction on number of testing images
+        # want to test on all testing images in final tests, not a subset
+        #if i >= opt.num_test:  # only apply our model to opt.num_test images.
+            #break
         
         model.set_input(data)  # unpack data from data loader
         model.test()           # run inference
