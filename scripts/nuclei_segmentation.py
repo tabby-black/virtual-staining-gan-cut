d0ada843dc4ec61c317a723d7126a28c185209b7
tabby-black <tb789@cam.ac.uk>
2026-04-28 12:50:19 +0100

    typo fix

--- scripts/nuclei_segmentation.py
+++ scripts/nuclei_segmentation.py
@@ -33,7 +33,7 @@ def create_nuclei_segmentation_mask(input_dir, output_dir):
 
 # create nuclei segmentation masks for all real RGB images
 # input directory is the same as using the real_B folder inside one of the model epoch results folders
-input_dir = "../datasets/histology_full/test_B"
+input_dir = "../datasets/histology_full/testB"
 output_dir = "../results/histology_analysis/real_RGB_nuclei_masks"
 create_nuclei_segmentation_mask(input_dir, output_dir)
 
