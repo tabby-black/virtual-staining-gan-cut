694e9905eff4e3b554742fd8fb1f6e422f1dc71d
tabby-black <tb789@cam.ac.uk>
2026-04-29 09:29:49 +0100

    resume rep2 testing

--- scripts/eval_to_wandb.py
+++ scripts/eval_to_wandb.py
@@ -189,7 +189,7 @@ if __name__ == "__main__":
     # CUT1 epochs: 0-101
     # CHANGE THIS LINE
     # plot every epoch for reporting runs
-    epochs = list(range(75, 205, 5))
+    epochs = list(range(130, 205, 5))
     wandb_project = "hyperspectral_image_reconstruction"
     # set to training run id each time so I can see metrics on the same run
     # currently set to id of run2 i.e. cyclegan_initial
