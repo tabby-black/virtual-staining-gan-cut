8a26e4477f17184983fd54ca7de74b9e3ed139fa
tabby-black <tb789@cam.ac.uk>
2026-04-29 22:36:33 +0100

    test CUT3

--- test.py
+++ test.py
@@ -95,8 +95,8 @@ if __name__ == '__main__':
         
         # removed restriction on number of testing images
         # want to test on all testing images in final tests, not a subset
-        #if i >= opt.num_test:  # only apply our model to opt.num_test images.
-            #break
+        if i >= opt.num_test:  # only apply our model to opt.num_test images.
+            break
         
         model.set_input(data)  # unpack data from data loader
         model.test()           # run inference
