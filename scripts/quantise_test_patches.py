c6fdbac03d2a8ffc7ee64c1b4959e42908c6674b
tabby-black <tb789@cam.ac.uk>
2026-04-29 14:17:30 +0100

    make 6-bit test dataset

--- scripts/quantise_test_patches.py
+++ scripts/quantise_test_patches.py
@@ -13,7 +13,7 @@ def quantise_patch(patch, bits):
 
 
 current_test_directory = "datasets/histology_full/testB"
-new_test_directory = "datasets/histology_full/testB_8b"
+new_test_directory = "datasets/histology_full/testB_6b"
 
 os.makedirs(new_test_directory, exist_ok=True)
 
@@ -31,7 +31,7 @@ for filename in os.listdir(current_test_directory):
     # calibration normalises hsi images but I haven't yet normalised rgb images - this is done by the data loader to [-1,1]
     img_np = np.array(img).astype(np.float32) / 255.0
     # quantise patch
-    img_q = quantise_patch(img_np, 8)
+    img_q = quantise_patch(img_np, 6)
     # convert back to [0,255]
     img_q = (img_q * 255).clip(0, 255).astype(np.uint8)
 
