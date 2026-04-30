506ff5b8a4fc18fd32fe57d0b285c32e5876aba2
tabby-black <tb789@cam.ac.uk>
2026-04-30 09:56:16 +0100

    resume rep2 testing

--- scripts/eval_to_wandb.py
+++ scripts/eval_to_wandb.py
@@ -129,10 +129,10 @@ def run_test_py(repo_root, experiment_name, dataroot, ep, results_dir):
         "python", "-u", "test.py",
         "--name", experiment_name,
         "--dataroot", dataroot,
-        "--CUT_mode", "CUT",
-        #"--model", "cycle_gan",
+        #"--CUT_mode", "CUT",
+        "--model", "cycle_gan",
         "--dataset_mode", "hsi_unaligned",
-        "--gpu_ids", "2",
+        "--gpu_ids", "0",
         "--epoch", str(ep),
        # CHANGE THIS LINE
         # only include this line for eval on training dataset
@@ -168,7 +168,7 @@ def run_test_py(repo_root, experiment_name, dataroot, ep, results_dir):
 if __name__ == "__main__":
     repo_root = "/local/scratch-3/tb789/projects/virtual-staining-gan-cut"
     # CHANGE THIS LINE
-    experiment_name = "hsi_to_rgb_CUT4"
+    experiment_name = "hsi_to_rgb_cyclegan_rep2"
     # so run this from /scripts
     # test on new test patches
     dataroot = "./datasets/histology_full/"
@@ -176,7 +176,7 @@ if __name__ == "__main__":
     # CUT1 epochs: 0-101
     # CHANGE THIS LINE
     # plot every epoch for reporting runs
-    epochs = list(range(0, 120, 5))
+    epochs = list(range(195, 205, 5))
     wandb_project = "hyperspectral_image_reconstruction"
     # set to training run id each time so I can see metrics on the same run
     # currently set to id of run2 i.e. cyclegan_initial
@@ -187,8 +187,8 @@ if __name__ == "__main__":
         project=wandb_project,
         #name=f"{experiment_name}_eval_{Path(dataroot).name}",
         # CHANGE THIS LINE
-        #id="wj8wjuut",
-        #resume = "allow"
+        id="8lkbozne",
+        resume = "allow"
     )
 
     for ep in epochs:
@@ -199,19 +199,19 @@ if __name__ == "__main__":
             ckpt_dir = Path(repo_root) / "checkpoints" / experiment_name
             # CHANGE THIS LINE
             # expected files for CycleGAN
-            #expected_files = [
-                #ckpt_dir / f"{ep}_net_D_A.pth",
-                #ckpt_dir / f"{ep}_net_D_B.pth",
-                #ckpt_dir / f"{ep}_net_G_A.pth",
-                #ckpt_dir / f"{ep}_net_G_B.pth",
-            #]
-            # expected files for CUT
             expected_files = [
-                ckpt_dir / f"{ep}_net_D.pth",
-                ckpt_dir / f"{ep}_net_E_B.pth",
-                ckpt_dir / f"{ep}_net_F.pth",
-                ckpt_dir / f"{ep}_net_G.pth",
+                ckpt_dir / f"{ep}_net_D_A.pth",
+                ckpt_dir / f"{ep}_net_D_B.pth",
+                ckpt_dir / f"{ep}_net_G_A.pth",
+                ckpt_dir / f"{ep}_net_G_B.pth",
             ]
+            # expected files for CUT
+            #expected_files = [
+                #ckpt_dir / f"{ep}_net_D.pth",
+                #ckpt_dir / f"{ep}_net_E_B.pth",
+                #ckpt_dir / f"{ep}_net_F.pth",
+                #ckpt_dir / f"{ep}_net_G.pth",
+            #]
 
             missing = [p.name for p in expected_files if not p.exists()]
             if missing:
