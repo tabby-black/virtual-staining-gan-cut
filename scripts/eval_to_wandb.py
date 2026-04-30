818df28a671039e6b26bd42dbc6334b3f3ec17c3
tabby-black <tb789@cam.ac.uk>
2026-04-30 10:41:29 +0100

    resume rep1a training

--- scripts/eval_to_wandb.py
+++ scripts/eval_to_wandb.py
@@ -133,7 +133,7 @@ def run_test_py(repo_root, experiment_name, dataroot, ep, results_dir):
         "--model", "cycle_gan",
         "--num_test", "846",
         "--dataset_mode", "hsi_unaligned",
-        "--gpu_ids", "0",
+        "--gpu_ids", "3",
         "--epoch", str(ep),
        # CHANGE THIS LINE
         # only include this line for eval on training dataset
@@ -169,7 +169,7 @@ def run_test_py(repo_root, experiment_name, dataroot, ep, results_dir):
 if __name__ == "__main__":
     repo_root = "/local/scratch-3/tb789/projects/virtual-staining-gan-cut"
     # CHANGE THIS LINE
-    experiment_name = "hsi_to_rgb_cyclegan_rep1"
+    experiment_name = "hsi_to_rgb_cyclegan_rep1a"
     # so run this from /scripts
     # test on new test patches
     dataroot = "./datasets/histology_full/"
@@ -177,7 +177,7 @@ if __name__ == "__main__":
     # CUT1 epochs: 0-101
     # CHANGE THIS LINE
     # plot every epoch for reporting runs
-    epochs = list(range(70, 205, 5))
+    epochs = list(range(15, 205, 5))
     wandb_project = "hyperspectral_image_reconstruction"
     # set to training run id each time so I can see metrics on the same run
     # currently set to id of run2 i.e. cyclegan_initial
@@ -188,7 +188,7 @@ if __name__ == "__main__":
         project=wandb_project,
         #name=f"{experiment_name}_eval_{Path(dataroot).name}",
         # CHANGE THIS LINE
-        id="wj8wjuut",
+        id="o074te17",
         resume = "allow"
     )
 
