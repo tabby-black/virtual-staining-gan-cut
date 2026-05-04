58dd356deb9deb80bcd10873690e1033dd4ca25d
tabby-black <tb789@cam.ac.uk>
2026-05-04 20:27:02 +0100

    .

--- scripts/test_cyclegan.sh
+++ scripts/test_cyclegan.sh
@@ -3,7 +3,7 @@ set -ex
 
 
 # CHANGE THIS LINE
-python test.py --dataroot /content/data/testB_8b --checkpoints_dir /content/checkpoints/hsi_to_rgb_cyclegan_rep2 --dataset_mode hsi_unaligned --model cycle_gan --num_test 846 --name hsi_to_rgb_cyclegan_rep2quant8 --epoch str(ep) --num_threads 0 --preprocess resize_and_crop --load_size 286 --batch_size 1 --crop_size 256 --no_dropout
+python test.py --dataroot /content/data/testB_8b --checkpoints_dir /content/checkpoints/hsi_to_rgb_cyclegan_rep2 --dataset_mode hsi_unaligned --model cycle_gan --num_test 846 --name hsi_to_rgb_cyclegan_rep2 --epoch str(ep) --num_threads 0 --preprocess resize_and_crop --load_size 286 --batch_size 1 --crop_size 256 --no_dropout
 # --CUT_mode CUT 
 # --model cycle_gan
 # --dataroot ./datasets/histology_full
