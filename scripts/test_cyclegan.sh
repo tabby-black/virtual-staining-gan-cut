8f4c71c6195751037b17893692f36a830ed31d76
tabby-black <tb789@cam.ac.uk>
2026-05-06 15:54:02 +0100

    last cyclegan quant test

--- scripts/test_cyclegan.sh
+++ scripts/test_cyclegan.sh
@@ -3,7 +3,7 @@ set -ex
 
 
 # CHANGE THIS LINE
-python test.py --dataroot ./datasets/histology_full --dataset_mode hsi_unaligned --name hsi_to_rgb_CUTrep1b --CUT_mode CUT --gpu_ids 2 --num_test 846 --epoch str(ep) --num_threads 0 --preprocess resize_and_crop --load_size 286 --batch_size 1 --crop_size 256 --no_dropout
+python test.py --dataroot ./datasets/histology_full_quant --dataset_mode hsi_unaligned --name hsi_to_rgb_cyclegan_rep2 --model cycle_gan --gpu_ids 2 --num_test 846 --epoch str(ep) --num_threads 0 --preprocess resize_and_crop --load_size 286 --batch_size 1 --crop_size 256 --no_dropout
 # --CUT_mode CUT 
 # --model cycle_gan
 # --dataroot ./datasets/histology_full
