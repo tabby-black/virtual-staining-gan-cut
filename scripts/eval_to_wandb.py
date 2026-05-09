8f6342766e8a8d78623a9367f393576293d20cb2
tabby-black <tb789@cam.ac.uk>
2026-05-09 13:05:57 +0100

    resume rep1 testing

--- scripts/eval_to_wandb.py
+++ scripts/eval_to_wandb.py
@@ -174,7 +174,7 @@ def checkpoint_size_mb(checkpoint_paths):
 if __name__ == "__main__":
     repo_root = "/local/scratch-3/tb789/projects/virtual-staining-gan-cut"
     # CHANGE THIS LINE
-    experiment_name = "hsi_to_rgb_CUTrep1"
+    experiment_name = "hsi_to_rgb_CUTrep1b"
     # so run this from /scripts
     # test on new test patches
     dataroot = "./datasets/histology_full/"
@@ -182,7 +182,7 @@ if __name__ == "__main__":
     # CUT1 epochs: 0-101
     # CHANGE THIS LINE
     # plot every epoch for reporting runs
-    epochs = list(range(135, 200, 5))
+    epochs = list(range(85, 200, 5))
     wandb_project = "hyperspectral_image_reconstruction"
     # set to training run id each time so I can see metrics on the same run
     # currently set to id of run2 i.e. cyclegan_initial
@@ -193,7 +193,7 @@ if __name__ == "__main__":
         project=wandb_project,
         #name=f"{experiment_name}_eval_{Path(dataroot).name}",
         # CHANGE THIS LINE
-        id="nkh634be",
+        id="79fgdl8e",
         resume = "allow"
     )
 
