e1fd46c38ccdcb8148f10871c08c66ec692b9a31
tabby-blackkinit <tb789@cam.ac.uk>
2026-05-12 19:47:13 +0100

    .

--- scripts/eval_to_wandb.py
+++ scripts/eval_to_wandb.py
@@ -142,7 +142,7 @@ def run_test_py(repo_root, experiment_name, dataroot, ep, results_dir):
         "--name", experiment_name,
         "--dataroot", dataroot,
         "--fp16",
-        "--result_suffix", "_16fp",
+        "--result_suffix", "_16fp_visuals",
         #"--CUT_mode", "CUT",
         "--model", "cycle_gan",
         "--num_test", "846",
